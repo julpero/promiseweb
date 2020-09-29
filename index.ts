d6a621548d050444feda7dd7b3e66395f21d9951
Juha Korhonen <julpero@gmail.com>
2020-09-29 15:28:22 +0300

    creating and joning games

--- index.ts
+++ index.ts
@@ -19,20 +19,109 @@ mongoUtil.connectToServer( function( err, client ) {
             console.log('user disconnected');
         });
 
+        socket.on('join game', async (newPlayer, fn) => {
+            var retVal = 'NOTSET';
+            console.log(newPlayer);
+            var ObjectId = require('mongodb').ObjectId;
+            var searchId = new ObjectId(newPlayer.gameId);
+            const database = mongoUtil.getDb();
+            const collection = database.collection('promiseweb');
+            const query = { gameStatus: 0,
+                            _id: searchId,
+                            password: newPlayer.gamePassword
+                             };
+            const game = await collection.findOne(query);
+            console.log(game);
+            if (game !== null) {
+                if (game.humanPlayersCount > game.humanPlayers.length && game.adminName != newPlayer.myName) {
+                    var nameFree = true;
+                    game.humanPlayers.forEach(function(player) {
+                        if (player.name == newPlayer.myName) nameFree = false;
+                    });
+
+                    if (nameFree) {
+                        var players = game.humanPlayers;
+                        players.push({name: newPlayer.myName, playerId: socket.id});
+                        const options = { upsert: true };
+                        const updateDoc = {
+                            $set: {
+                                humanPlayers: players,
+                            }
+                        };
+                        const result = await collection.updateOne(query, updateDoc, options);
+                        // console.log(result);
+                        retVal = 'OK';
+                    } else {
+                        retVal = 'NAMENOTOK';
+                    }
+                } else {
+                    retVal = 'NOTVALID';
+                }
+            } else {
+                retVal = 'GAMENOTFOUND';
+            }
+
+            fn(retVal);
+
+            console.log(retVal);
+            if (retVal == 'OK') {
+                // let's update info to clients
+                const val = await collection.findOne(query);
+                var resultGameInfo = {
+                    id: val._id,
+                    humanPlayersCount: val.humanPlayersCount,
+                    computerPlayersCount: val.botPlayersCount,
+                    startRound: val.startRound,
+                    turnRound: val.turnRound,
+                    endRound: val.endRound,
+                    humanPlayers: val.humanPlayers,
+                    hasPassword: val.password.length > 0,
+                };
+                if (resultGameInfo.humanPlayersCount == resultGameInfo.humanPlayers.length) {
+                    // start game
+                    io.emit('start game', resultGameInfo);
+                } else {
+                    io.emit('update gameinfo', resultGameInfo);
+                }
+                
+            }
+            
+        });
+
         socket.on('create game', async (gameOptions) => {
             console.log(gameOptions);
             const database = mongoUtil.getDb();
             const collection = database.collection('promiseweb');
+            gameOptions.humanPlayers = [{name: gameOptions.adminName, playerId: socket.id}];
+
             const result = await collection.insertOne(gameOptions);
             console.log('gameOptions inserted ' + result.insertedCount + ' with _id: ' + result.insertedId);
         });
 
-        socket.on('get games', async () => {
+        socket.on('get games', async (data, fn) => {
             console.log('start to get games');
             const database = mongoUtil.getDb();
             const collection = database.collection('promiseweb');
             const query = { gameStatus: 0 };
-            const games = await collection.findOne(query);
+            const cursor = await collection.find(query);
+
+            var games = [];
+            await cursor.forEach(function(val) {
+                games.push({
+                    id: val._id,
+                    humanPlayersCount: val.humanPlayersCount,
+                    computerPlayersCount: val.botPlayersCount,
+                    startRound: val.startRound,
+                    turnRound: val.turnRound,
+                    endRound: val.endRound,
+                    humanPlayers: val.humanPlayers,
+                    hasPassword: val.password.length > 0,
+                });
+            });
+                
+
+            fn(games);
+            // socket.emit('game list', games);
             console.log(games);
         });
     });
--- promiseweb.js
+++ promiseweb.js
@@ -11,24 +11,20 @@ function validateNewGame(gameOptions) {
         alert('End round must be equal or greater than turn round');
         return false;
     }
-    if (gameOptions.humanPlayersCount > 0 && gameOptions.playerName.length  < 3) {
+    if (gameOptions.humanPlayersCount > 0 && gameOptions.adminName.length  < 3) {
         alert('Your (nick)name must be at least four characters long');
         return false;
-    }
-    if (gameOptions.humanPlayersCount > 0) {
-        gameOptions.humanPlayers = [gameOptions.playerName];
     }
     return true;
 }
 
-function createNewGame(gameOptions) {
-    debugger;
+function createNewGame(socket, gameOptions) {
     console.log(gameOptions);
-    var socket = io();
+    //var socket = io();
     socket.emit('create game', gameOptions);
 }
 
-function initcreateNewGameButton() {
+function initcreateNewGameButton(socket) {
     $('#createNewGameButton').on('click', function() {
         var gameOptions = {
             humanPlayersCount: parseInt($('#newGameHumanPlayersCount option:selected')[0].value, 10),
@@ -36,22 +32,89 @@ function initcreateNewGameButton() {
             startRound: parseInt($('#newGameStartRound option:selected')[0].value, 10),
             turnRound: parseInt($('#newGameTurnRound option:selected')[0].value, 10),
             endRound: parseInt($('#newGameEndRound option:selected')[0].value, 10),
-            playerName: $('#newGameMyName').val(),
+            adminName: $('#newGameMyName').val(),
             password: $('#newGamePassword').val(),
             gameStatus: 0,
         };
-        if (validateNewGame(gameOptions)) createNewGame(gameOptions);
-    });
-}
-function initGetGamesButton() {
-    $('#getGamesButton').on('click', function() {
-        console.log('getting games');
-        var socket = io();
-        socket.emit('get games');
+        if (validateNewGame(gameOptions)) createNewGame(socket, gameOptions);
     });
 }
 
-function initButtons() {
-    initcreateNewGameButton();
-    initGetGamesButton();
+function gamePlayersToStr(players, totalHumans, totalComputers) {
+    var retStr = '';
+    players.forEach(function (player) {
+        retStr+= player.name + ', ';
+    });
+    for (i = players.length; i < totalHumans; i++) retStr+= '{}, ';
+    if (retStr.length > 2) retStr = retStr.substring(0, retStr.length-2);
+    return retStr;
+}
+
+function validateJoinGame(gameDetails) {
+    return true;
+}
+
+function joinGame(socket, id) {
+    //var socket = io();
+    var gameDetails = { gameId: id,
+        myName: $('#myName'+id).val(),
+        gamePassword: $('#password'+id).val(),
+    };
+    if (validateJoinGame(gameDetails)) {
+        socket.emit('join game', gameDetails, function (response) {
+            console.log(response);
+
+        });
+    }
+}
+
+function showGames(socket, gameList) {
+    var gameListContainer = $('#joinGameCollapse');
+    gameList.forEach(function (game) {
+        var gameContainer = $('<div id="game' + game.id + '">').addClass('card card-body');
+        var gameContainerDiv = $('<div>').addClass('row');
+        gameContainerDiv.append($('<div>').addClass('col-2').text(game.startRound + '-' + game.turnRound + '-' + game.endRound));
+        gameContainerDiv.append($('<div id="gamePlayers' + game.id + '">').addClass('col-3').text(gamePlayersToStr(game.humanPlayers, game.humanPlayersCount, game.computerPlayersCount)));
+        gameContainerDiv.append(($('<div>').addClass('col-3').append($('<input type="text" id="myName'+game.id+'">'))));
+        gameContainerDiv.append(($('<div>').addClass('col-3').append($('<input disabled type="text" id="password'+game.id+'">'))));
+        var btnId = 'joinGameButton' + game.id;
+        var joinGameButton = ($('<button id="'+btnId+'">').addClass('btn btn-primary').text('Join game'));
+        gameContainerDiv.append(($('<div>').addClass('col-1')).append(joinGameButton));
+
+        gameListContainer.append(gameContainer.append(gameContainerDiv));
+
+        $('#'+btnId).on('click', function() {
+            joinGame(socket, game.id);
+        });
+
+        console.log(game);
+    });
+}
+
+function initGameListEvent(socket) {
+    $('#joinGameCollapse').on('shown.bs.collapse', function () {
+        //var socket = io();
+        socket.emit('get games', {}, function (response) {
+            console.log(response);
+            showGames(socket, response);
+        });
+    });
+
+    $('#joinGameCollapse').on('hidden.bs.collapse', function () {
+        const node = document.getElementById('joinGameCollapse');
+        node.innerHTML = '';
+    });
+}
+
+function initButtons(socket) {
+    initcreateNewGameButton(socket);
+}
+
+function initEvents(socket) {
+    initGameListEvent(socket);
+}
+
+function mainInit(socket) {
+    initEvents(socket);
+    initButtons(socket);
 }
