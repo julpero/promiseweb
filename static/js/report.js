26e7314045fbbbf3ecffe743d0ff8fb65a1d3111
Juha Korhonen <julpero@gmail.com>
2020-12-01 18:36:48 +0200

    some code cleaning

--- static/js/report.js
+++ static/js/report.js
@@ -73,13 +73,7 @@ function showGames(gameList) {
     });
 
     $('.reportGameButton').on('click', function() {
-        var getReportObj = { gameId: this.value };
-        socket.emit('get game report', getReportObj, function(gameReportData) {
-            console.log(gameReportData);
-            showOneGameReport(gameReportData);
-            showOneKeepsReport(gameReportData);
-            $('#oneGameReportModal').modal('toggle');
-        });
+        getOneGameReport(this.value);
     });
 }
 
@@ -140,55 +134,6 @@ function showAveragePointsPerGames(reportObject) {
             }
         },
         bar: { groupWidth: "90%" }
-    };
-    var chart = new google.charts.Bar(document.getElementById(reportIdName));
-    chart.draw(reportData, google.charts.Bar.convertOptions(options));
-}
-
-function showOneGameReport(reportObject) {
-    var reportIdName = 'oneGameReportBody';
-    var reportData = new google.visualization.DataTable();
-    reportData.addColumn('number', 'Round');
-    reportObject.players.forEach(function(player) {
-        reportData.addColumn('number', player);
-    });
-    reportData.addRows(reportObject.points);
-
-    var options = {
-        height: 500,
-        width: 1000,
-        legend: { position: 'right' },
-        chart: {
-            title: 'Points in game',
-            subtitle: 'cumulative points per round',
-        },
-        axes: {
-            x: {
-                0: { side: 'bottom', label: 'Round'}
-            }
-        },
-    };
-    var chart = new google.charts.Line(document.getElementById(reportIdName));
-    chart.draw(reportData, google.charts.Line.convertOptions(options));
-}
-
-function showOneKeepsReport(reportObject) {
-    var reportIdName = 'oneGameKeepsBody';
-    var reportDataArr = [['Name', 'Keeps']];
-    for (var i = 0; i < reportObject.players.length; i++) {
-        reportDataArr.push([reportObject.players[i], reportObject.keeps[i]]);
-    }
-    var reportData = new google.visualization.arrayToDataTable(reportDataArr);
-    var options = {
-        height: 200,
-        width: 1000,
-        legend: { position: 'none' },
-        chart: {
-            title: 'Keeps in game',
-            subtitle: 'number of keeps by nickname',
-        },
-        bar: { groupWidth: "80%" },
-        bars: 'horizontal',
     };
     var chart = new google.charts.Bar(document.getElementById(reportIdName));
     chart.draw(reportData, google.charts.Bar.convertOptions(options));
