ece045a0ad9b3d3e7a6a959ca253ca2e02563a53
Juha Korhonen <julpero@gmail.com>
2022-02-03 09:54:21 +0200

    lint

--- aiPlayer.js
+++ aiPlayer.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 module.exports = {
     getRandomAiPlayers: function(numberOfAiPlayers) {
         let aiPlayerArr = [];
